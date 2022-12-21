1d72c918a085623feb595219b356714410457abe
AlomX <garbin.maxence@gmail.com>
2022-12-21 17:59:48 +0100

    graphic on work

--- resources/js/Pages/Monitor/Partials/MonitorList.vue
+++ resources/js/Pages/Monitor/Partials/MonitorList.vue
@@ -2,6 +2,7 @@
 import { onMounted, onUnmounted, ref } from 'vue';
 import { computed } from 'vue';
 import { Link } from '@inertiajs/inertia-vue3';
+import { createPopper } from '@popperjs/core';
 
 const props = defineProps({
     monitors: {
@@ -10,7 +11,7 @@ const props = defineProps({
 });
 
 onMounted(() => {
-    console.log(props.monitors);
+    console.log(props.monitors);    
 });
 </script>
 
@@ -18,13 +19,67 @@ onMounted(() => {
     <div class="grid grid-cols-2 gap-4 pt-6">
         <div class="p-4 sm:p-8 bg-white dark:bg-slate-800 dark:text-slate-200 shadow sm:rounded-lg flex items-center" 
             v-for="monitor in props.monitors" :key="monitor.id">
-                <div class="shrink-0 mr-7">
+                <div class="flex-none shrink-0 mr-7">
                     <img class="h-12 w-12" :src="'http://www.google.com/s2/favicons?domain=' + monitor.address + '&sz=128'" :alt="monitor.address + ' Logo'">
                 </div>
-                <div>
-                    <div class="text-xl">{{ monitor.name }} <span class="text-base text-slate-500"> ( {{ monitor.address }} ) </span></div>
-                    <p class="text-slate-500">{{ monitor.address }}</p>
+                <div class="flex-auto flex flex-wrap">
+                    <a class="flex-none" :href="'https://' + monitor.address" target="_blank">
+                        <span class="text-xl">{{ monitor.name }}</span>
+                        <span class="text-slate-500"> ( {{ monitor.address }} ) </span>
+                    </a>
+                    <span class="flex-auto grow text-right text-slate-500">last response : </span>
+                    <div class="flex-none w-full flex gap-x-0.5">
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-red-700 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-amber-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-amber-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-lime-500 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-red-700 rounded cursor-help">&nbsp;</div>
+                        <div class="flex-auto bg-red-700 rounded cursor-help">&nbsp;</div>
+                    </div>
                 </div>
+
         </div>
     </div>
 </template>
